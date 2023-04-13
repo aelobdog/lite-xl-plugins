c1f17b741b2b0e79cbf1b7a5f06c589ced572d4b
Ashwin Godbole <dev.godboleashwin@gmail.com>
2023-04-13 14:35:19 +0530

    heartburn theme for lite-xl

---
+++ heartburn.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background         = { common.color "#252525" }
+style.background2        = { common.color "#191919" }
+style.background3        = { common.color "#202020" }
+style.text               = { common.color "#EDE1D6" }
+style.caret              = { common.color "#FFA0A0" }
+style.accent             = { common.color "#FFA0A0" }
+style.dim                = { common.color "#828282" }
+style.divider            = { common.color "#373737" }
+style.selection          = { common.color "#523232" }
+style.line_number        = { common.color "#656565" }
+style.line_number2       = { common.color "#FFA0A0" }
+style.line_highlight     = { common.color "#252545" }
+style.scrollbar          = { common.color "#353535" }
+style.scrollbar2         = { common.color "#353535" }
+
+style.syntax["normal"]   = { common.color "#dcdccc" }
+style.syntax["symbol"]   = { common.color "#dcdccc" }
+style.syntax["comment"]  = { common.color "#7f9f7f" }
+style.syntax["keyword"]  = { common.color "#f0dfaf" }
+style.syntax["keyword2"] = { common.color "#dfdfbf" }
+style.syntax["number"]   = { common.color "#8cd0d3" }
+style.syntax["literal"]  = { common.color "#dfaf8f" }
+style.syntax["string"]   = { common.color "#cc9393" }
+style.syntax["operator"] = { common.color "#f0efd0" }
+style.syntax["function"] = { common.color "#badd90" }
